15b3ed1aef204303a53d1aacf9d096ac1bc90fb9
Chase Albert <chase@volume.finance>
2023-05-03 06:25:54 -0600

    Use postgres for the cache

--- run_momentum_table.py
+++ run_momentum_table.py
@@ -3,36 +3,25 @@ from sys import argv
 import cache_db
 from momentum_scanner_intraday import get_high_returns
 
-import sentry_sdk
 
-sentry_sdk.init(
-    dsn="https://955ac0a74d244e2c914767a351d4d069@o1200162.ingest.sentry.io/4505082653573120",
-    traces_sample_rate=1.0,
-)
+##sentry_sdk.init(
+##    dsn="https://955ac0a74d244e2c914767a351d4d069@o1200162.ingest.sentry.io/4505082653573120",
+##    traces_sample_rate=1.0,
+##)
 
 
 def main():
     dex = argv[1]
     lag_return = int(argv[2])
     daily_volume = int(argv[3])
-    monthly_mean_volume = argv[4]
-    liquidity = argv[5]
 
     def recompute():
-        df = get_high_returns(
-            dex, lag_return, daily_volume, monthly_mean_volume, liquidity
-        )
+        df = get_high_returns(dex, lag_return, daily_volume)
         df.dropna(how="all", axis=1, inplace=True)
         return df.to_json()
 
-    output = cache_db.fetch(
-        f"get_high_returns({dex}, {lag_return}, {daily_volume}, {monthly_mean_volume}, {liquidity})",
-        recompute,
-    )
-
+    output = cache_db.fetch(dex, lag_return, daily_volume, recompute)
     print(output)
-    with open("data.json", "w", encoding="utf-8") as f:
-        f.write(output)
 
 
 if __name__ == "__main__":
