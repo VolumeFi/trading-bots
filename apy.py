5398a96518f338d7d3962860912026004796d555
fieldtheory123 <biyaqiao@hotmail.com>
2023-06-13 11:44:48 +0800

    Add a test and some edits

--- apy.py
+++ apy.py
@@ -21,21 +21,18 @@ def return_per_trade(entry_usd, exit_usd):
 
 def get_trades_per_user(user_id):
     """
-    TODO:
+    TODO: write this function to query users trades  by their ids in the form defined below 
     """
-    trades = {
-        "id1": {
-            "entry_timestamp": None,
-            "entry_usd": None,
-            "exit_timestamp": None,
-            "exit_usd": None,
-        }
-    }
+    trades = {'id1':{'entry_timestamp':'2023-06-12 00:00:00','entry_usd':110,
+                 'exit_timestamp':'2023-06-12 12:00:00','exit_usd':120},
+         'id2':{'entry_timestamp':'2023-06-12 13:00:00','entry_usd':125,
+                 'exit_timestamp':'2023-06-12 23:00:00','exit_usd':120}}
+
     return trades
 
 
 def get_returns(trades: dict):
-    rets = pd.Seris()
+    rets = pd.Series()
     for trade_id in trades:
         entry_time = pd.to_datetime(trades[trade_id]["entry_timestamp"])
         entry_usd = trades[trade_id]["entry_usd"]
@@ -53,12 +50,15 @@ def get_duration(start, end):
 
 
 def get_apy(user_id):
-    """
-    TODO
-    """
     trades = get_trades_per_user(user_id)
     rets = get_returns(trades)
     cumret = rets.sum()
     duration = get_duration(rets.index[0], rets.index[-1])
-    cumret_annual = cumret * 365 / duration
+    if duration != 0:
+        cumret_annual = cumret * 365 / duration
+    else:
+        cumret_annual = 0
     return cumret, cumret_annual
+
+if __name__ == '__main__':
+    print(get_apy(''))
